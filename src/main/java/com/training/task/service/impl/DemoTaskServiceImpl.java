70f2d96f509b963914ceb0a1e25a10de261c4516
LuWenhui <1265400024@qq.com>
2016-11-04 15:49:02 +0800

    （八）log扩展logback.xml级别

--- src/main/java/com/training/task/service/impl/DemoTaskServiceImpl.java
+++ src/main/java/com/training/task/service/impl/DemoTaskServiceImpl.java
@@ -3,6 +3,7 @@ package com.training.task.service.impl;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,8 @@ import com.training.task.service.DemoTaskService;
 @Component
 public class DemoTaskServiceImpl implements DemoTaskService {
 
+	private Logger logger = Logger.getLogger(DemoTaskServiceImpl.class);
+	
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Scheduled(fixedRate = 5000)
@@ -21,6 +24,11 @@ public class DemoTaskServiceImpl implements DemoTaskService {
     @Scheduled(cron="0/5 * *  * * ? ")
     public void getTask2() {
     	System.out.println("任务2，当前时间：" + dateFormat.format(new Date()));
+    }
+    
+    @Scheduled(cron="0/5 * *  * * ? ")
+    public void getTaskLog4j() {
+    	logger.info("Log4j，当前时间：" + dateFormat.format(new Date()));
     }
     
 }
